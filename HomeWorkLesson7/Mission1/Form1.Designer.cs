4316a335f7ade4b94e179afb1c1939fe69b42407
enemy <nkalmykov937@gmail.com>
2019-02-16 12:19:20 +0300

    1

---
+++ HomeWorkLesson7/Mission1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Mission1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb = new System.Windows.Forms.TextBox();
+            this.bttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(189, 69);
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(231, 20);
+            this.tb.TabIndex = 0;
+            // 
+            // bttn
+            // 
+            this.bttn.Location = new System.Drawing.Point(189, 95);
+            this.bttn.Name = "bttn";
+            this.bttn.Size = new System.Drawing.Size(134, 23);
+            this.bttn.TabIndex = 1;
+            this.bttn.Text = "button1";
+            this.bttn.UseVisualStyleBackColor = true;
+            this.bttn.Click += new System.EventHandler(this.bttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 434);
+            this.Controls.Add(this.bttn);
+            this.Controls.Add(this.tb);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.Button bttn;
+    }
+}
+
---
+++ HomeWorkLesson7/Mission1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Калмыков
+
+//2. Используя Windows Forms, разработать игру «Угадай число». 
+//Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток. 
+//Для ввода данных от человека используется элемент TextBox.
+
+namespace Mission1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Random rnd = new Random();
+            int i = rnd.Next(0, 100);
+
+        }
+    }
+}
