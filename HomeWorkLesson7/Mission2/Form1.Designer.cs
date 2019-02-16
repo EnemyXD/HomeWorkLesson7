88d0c67f0cf01c48916cedaa31fd3c634e748706
enemy <nkalmykov937@gmail.com>
2019-02-16 13:18:59 +0300

    Выполинл задание номер 1

---
+++ HomeWorkLesson7/Mission2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Mission2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblCmdCounter = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(194, 71);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(13, 13);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "0";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(283, 61);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+1";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(283, 107);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(75, 23);
+            this.btnMulti.TabIndex = 2;
+            this.btnMulti.Text = "*2";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(283, 152);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblCmdCounter
+            // 
+            this.lblCmdCounter.AutoSize = true;
+            this.lblCmdCounter.Location = new System.Drawing.Point(599, 378);
+            this.lblCmdCounter.Name = "lblCmdCounter";
+            this.lblCmdCounter.Size = new System.Drawing.Size(98, 13);
+            this.lblCmdCounter.TabIndex = 4;
+            this.lblCmdCounter.Text = "Всего комманд: 0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AllowDrop = true;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolMenu
+            // 
+            this.toolMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.toolMenu.Name = "toolMenu";
+            this.toolMenu.Size = new System.Drawing.Size(53, 20);
+            this.toolMenu.Text = "Меню";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            this.игратьToolStripMenuItem.Click += new System.EventHandler(this.игратьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCmdCounter);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblCmdCounter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolMenu;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ HomeWorkLesson7/Mission2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mission2
+{
+    public partial class Form1 : Form
+    {
+
+        public static Random rnd = new Random();
+        public static int goal = 0;
+        public static int cmdCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+
+            cmdCounter++;
+            lbl.Text = (int.Parse(lbl.Text) + 1).ToString();
+            lblCmdCounter.Text = "Всего комманд: " + cmdCounter;
+            if (int.Parse(lbl.Text) == goal)
+            {
+                MessageBox.Show($"Вы набрали необходимое число за {cmdCounter} ходов.", "", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+
+            cmdCounter++;
+            lbl.Text = (int.Parse(lbl.Text) * 2).ToString();
+            lblCmdCounter.Text = "Всего комманд: " + cmdCounter;
+            if (int.Parse(lbl.Text) == goal)
+            {
+                MessageBox.Show($"Вы набрали необходимое число за {cmdCounter} ходов.", "", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            cmdCounter++;
+            lbl.Text = "0";
+            lblCmdCounter.Text = "Всего комманд: " + cmdCounter;
+
+        }     
+
+        private void игратьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            lbl.Text = "0";
+            goal = rnd.Next(1, 100);
+            MessageBox.Show($"Игра началась. Наберите число {goal} за минимальное колличество ходов", "", MessageBoxButtons.OK);
+            lblCmdCounter.Text = "0";
+            cmdCounter = 0;
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
